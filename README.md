f037897fdefe2aeae710ac8a3fbc9a867903ea0e
Alexander <pchp250g8@tutamail.com>
2026-02-21 21:44:49 +0300

    Created Project Task-2-3

---
+++ README.md
@@ -0,0 +1,7 @@
+RobotTask-2-3. Python Tutorials. Module Robot. StepInDev Platform. GUI Application. 
+
+Robot-Pos-N1
+
+
+
+Robot-Pos-N2
---
+++ Task-2-3.py
@@ -0,0 +1,8 @@
+from robot import *
+
+while is_wall_left():
+    paint()
+    move_down()
+    move_right()
+    move_up()
+move_left()
